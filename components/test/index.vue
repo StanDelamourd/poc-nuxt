e9ca1c57db8f9dd623a3e7b7ecb28d268a961194
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-14 16:33:12 +0100

    Add test component

---
+++ components/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{title}}</p>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
---
+++ pages/inscription/index.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <Test v-for="item in items" :key="item.title" :title="item.title"/>
+  <p v-if="isDisplay">{{title}}</p>
+  <p v-else>ookok</p>
+</div>  
+
+</template>
+
+<script>
+import Test from '../../components/test/index';
+export default {
+data () {
+    return {
+      isDisplay: false,
+      title: 'Vuetify.js',
+      items: [
+        {
+          title: 'first'
+        },
+        {
+          title: 'first'
+        }
+      ]
+    }
+  },
+  components:{
+    Test
+  }
+}
+</script>
