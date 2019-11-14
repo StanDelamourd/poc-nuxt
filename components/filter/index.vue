1b934fa59283409c79e3096fa437ca25f963a077
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-14 23:06:11 +0100

    Add filter component

---
+++ components/filter/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <select>
+    <option v-for="data in datas" :key="data.value" :value="data.value">{{data.value}}</option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'filter',
+  props: {
+    datas: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+select{
+    background: grey;
+    color: black;
+    width: 100%;
+    height: 5vh;
+    margin-top: 1vh;
+  }
+  option {
+    color: black;
+  }
+</style>
--- pages/offers/index.vue
+++ pages/offers/index.vue
@@ -1,12 +1,116 @@
 <template>
-  <h1>Offers</h1>  
+  <div>
+    <div class="complete">
+      <p>Completez votre profil avec votre diplome et votre CV pour acceder aux offres d'emplois</p>
+      <Button title="Ajouter mes pieces"/>
+    </div>
+    <div class="filters">
+      <input type="search">
+      <div class="filters__select">
+        <Filter :datas="times" />
+        <Filter :datas="missions" />
+      </div>
+    </div>
+    <div class="offers">
+
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import Button from '../../components/button';
+import Filter from '../../components/filter';
 
+export default {
+  components:{
+    Button,
+    Filter
+  },
+  data(){
+    return {
+      times:[
+        {
+          value: '6 mois'
+        },
+        {
+          value: '12 mois'
+        },
+        {
+          value: '18 mois'
+        },
+        {
+          value: '24 mois'
+        }
+      ],
+      missions: [
+        {
+          value: 'Infirmier'
+        },
+        {
+          value: 'Medecin'
+        },
+        {
+          value: 'Educateur(rice)'
+        }
+      ],
+      offers: [
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        },
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        },
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        },
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        },
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        },
+        {
+          profession: 'Infirmier',
+          place: 'EHPAD, Bordeaux',
+          type: 'CDD',
+          time: '18 mois'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .complete{
+    margin-top: 2vh;
+    width: 100%;
+    padding: 15px;
+    background: rgba(125, 125, 125, .5);
+    text-align: center; 
+  }
+  .complete p {
+    padding-bottom: 2vh;
+  }
+  input {
+    width: 100%;
+    border: 1px solid black;
+    height: 5vh;
+    margin-top: 1vh;
+  }
 </style>
