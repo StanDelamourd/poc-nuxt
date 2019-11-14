3ca1a2f49e52b611e4eaee9642b282a36903e470
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-14 22:06:36 +0100

    Change wizard form step when the user click on next button

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="form">
       <form>
-        <!-- <FirstStep/> -->
-        <!-- <SecondStep/> -->
-        <!-- <ThirdStep/> -->
-        <!-- <FourthStep/> -->
-        <!-- <FivethStep/> -->
-        <SixthStep/>
+        <FirstStep v-if="step === 1"/>
+        <SecondStep v-else-if="step === 2"/>
+        <ThirdStep v-else-if="step === 3"/>
+        <FourthStep v-else-if="step === 4"/>
+        <FivethStep v-else-if="step === 5"/>
+        <SixthStep v-else-if="step === 6"/>
       </form>
     </div>
-    <Button title="Suivant" />
+    <Button title="Suivant" :clickEvent="changeStep"/>
   </div>  
 </template>
 <script>
@@ -26,7 +26,7 @@ import Button from '../../components/button';
 export default {
   data () {
     return {
-
+      step: 1,
     }
   },
   components:{
@@ -37,6 +37,11 @@ export default {
     FivethStep,
     SixthStep,
     Button,
+  },
+  methods:{
+    changeStep(){
+      this.step += 1;
+    }
   }
 }
 </script>
