581d7b76d291fa9d6a3d3823cc3d2fa47d1bbf20
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-14 21:12:42 +0100

    Add fourthStep to inscription page

---
+++ components/fourthStep/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Metier</h1>
+    <h2>Dans quel type de metier aimeriez-vous travailler ?</h2>
+    <Field :value="situation" label="Situation" :datas="situations"/>
+  </div>
+</template>
+
+<script>
+import Field from '../field';
+export default {
+  name: 'thirdStep',
+  components: {
+    Field
+  },
+  data(){
+    return {
+      situation: '',
+      situations: [
+        {
+          title: 'Chomage'
+        },
+        {
+          title: 'Sans emploi'
+        }
+      ]
+    };
+  }
+}
+</script>
+<style scoped>
+h1, h2 {
+  text-align: center;
+  font-weight: 500;
+}
+h1 {
+  font-size: 1.5em;
+}
+h2 {
+  font-size: 1em;
+  margin-bottom: 10vh;
+}
+</style>
