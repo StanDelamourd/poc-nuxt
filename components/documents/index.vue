681d837275364d28f9d27b8b816667594c45fd00
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-15 09:40:04 +0100

    Add documnents component

---
+++ components/documents/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <li>CV</li>
+      <li>Diplome</li>
+    </ul>
+    <Button label="Ajouter mes pieces"/>
+  </div>
+</template>
+
+<script>
+import Button from '../button';
+export default {
+  name: 'infos',
+  components:{
+    Button
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    width: 90%;
+    margin: auto;
+  }
+</style>
