8fc450b5b7dff8e8c6fdaa95513c052a956a992a
j.bourel.ext_azu <calaruni@hotmail.com>
2019-11-15 09:39:00 +0100

    Add infos component

---
+++ components/infos/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li>Metier</li>
+      <li>Ville</li>
+      <li>Coordonnees</li>
+      <li>Permis</li>
+      <li>Vehicule</li>
+    </ul>
+    <Button label="Completez mes infos"/>
+  </div>
+</template>
+
+<script>
+import Button from '../button';
+export default {
+  name: 'infos',
+  components:{
+    Button
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    width: 90%;
+    margin: auto;
+  }
+</style>
